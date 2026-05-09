6a5a4df5622dd76f24a068701c9dee2fc6ce747f
Kunjut <tembulats2020@gmail.com>
2026-05-10 03:33:25 +0500

    Initial: Modular Monolith structure with Docker and Postgres

---
+++ OrbitX/Controllers/TLEController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrbitX.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TLEController : ControllerBase
+    {
+
+    }
+}
